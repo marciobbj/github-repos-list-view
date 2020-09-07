5461cc8e7bead88405d8f87f09823aadf9cc0b88
Bernardes <marciobernardes@live.com>
2020-09-07 13:48:22 -0300

    adds List component and simple mocked repos

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,21 +1,62 @@
-import React, { useState }from 'react';
+import React, { useState, Component }from 'react';
 import './App.css';
 import GlobalStyle from './styles/global'
 import Search from './components/search'
 import Result from './components/result'
+import Container from './components/container'
+import List from './components/list'
+
+
+class Main extends Component {
+  state = {
+    repositories: [
+      {
+        name: 'facebook/react',
+        owner: {
+          name: 'facebook',
+          avatar_url: 'https://avatars3.githubusercontent.com/u/69631?v=4',
+        },
+      },
+    ],
+    loading: false,
+    error: false,
+    errorMessage: '',
+  };
+
+  render() {
+    const { repositories } = this.state;
+
+    return (
+      <Container>
+        <header className="App-header">
+          <h3 style={{margin: "10px 0"}}>Search Github: </h3>
+          <Search />
+        </header>
+        
+        <List>
+          {repositories.map(repo => (
+            <li key={repo.name}>
+              <div>
+                <a href='/'>
+                  <img src={repo.owner.avatar_url} alt={repo.owner.name} />
+                  <span>{repo.name}</span>
+                </a>
+              </div>
+            </li>
+          ))}
+        </List>
+
+      </Container>
+    )
+  }
+}
+
 
 function App() {
   return (
     <div className="App">
       <GlobalStyle></GlobalStyle>
-      <header className="App-header">
-        <h1>Search Github repositories:</h1>
-        <Search />
-      </header>
-
-      <div className='ListView'>
-        <Result />
-      </div>
+      <Main />
     </div>
   );
 }
